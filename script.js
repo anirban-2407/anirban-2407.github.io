c730740999d3939824876b8255947508eacf3363
anirban-2407 <158179475+anirban-2407@users.noreply.github.com>
2026-02-19 20:14:01 +0530

    Fix syntax errors in script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-<!-- ======================= script.js ======================= -->
-// Highlight active menu link automatically
 const currentPage = location.pathname.split("/").pop();
 
 document.querySelectorAll(".menu a").forEach(link => {
